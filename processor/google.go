c9d54c522fcc6888a00419b3c258f2a1c3d46f7d
Dmitry Matveyev <d.matveyev@intetics.com>
2018-08-06 17:19:31 +0300

    Obtain sheet ID and cell range from message

--- processor/google.go
+++ processor/google.go
@@ -1,11 +1,34 @@
 package processor
 
-import "gsheets-slack-chatbot/model/google"
+import (
+	"fmt"
+	"gsheets-slack-chatbot/model/google"
+	"regexp"
+)
 
 func (p *Processor) getCellContent(message string) (string, error) {
-	return message, nil
+	props, err := p.getCellProps(message)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%s %s", props.SheetID, props.Range), nil
 }
 
-func getCellProps(message string) (model.CellProps, error) {
-	return model.CellProps{}, nil
+func (p *Processor) getCellProps(message string) (model.CellProps, error) {
+	pattern, err := p.config.Get("GoogleSheetsCellExpr")
+	if err != nil {
+		return model.CellProps{}, err
+	}
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		return model.CellProps{}, err
+	}
+
+	props := re.FindStringSubmatch(message)
+	if len(props) != 3 {
+		return model.CellProps{}, fmt.Errorf("not matched")
+	}
+
+	return model.CellProps{SheetID: props[1], Range: props[2]}, nil
 }
